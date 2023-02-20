97cf1403bb90b36ca743b7b07c7efd7ea3190959
Bilal Atique <74999164+BilalAtique@users.noreply.github.com>
2023-02-20 20:30:08 +0500

    Converted scss to style components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,9 +1,10 @@
-import "./cart-dropdown.styles.scss";
+import "./cart-dropdown.styles";
 import Button from "../button/button.component";
 import CartItem from "../cart-tem/cart-item.component";
 import { useContext } from "react";
 import { CartContext } from "../../context/cart.context";
 import { useNavigate } from "react-router-dom";
+import { CartDropdownContainer, CartItems, EmptyMessage } from "./cart-dropdown.styles";
 
 const CartDropdown = () => {
     const navigate = useNavigate();
@@ -12,14 +13,18 @@ const CartDropdown = () => {
 
     const { cartItems } = useContext(CartContext);
     return (
-        <div className="cart-dropdown-container">
-            <div className="cart-items">
-                {cartItems.map((item) => (
-                    <CartItem key={item.id} cartItem={item} />
-                ))}
-            </div>
+        <CartDropdownContainer>
+            <CartItems>
+                {cartItems.length ? (
+                    cartItems.map((item) => (
+                        <CartItem key={item.id} cartItem={item} />
+                    ))
+                ) : (
+                    <EmptyMessage>Your cart is empty</EmptyMessage>
+                )}
+            </CartItems>
             <Button onClick={goToCheckoutHandler}>Go to checkout</Button>
-        </div>
+        </CartDropdownContainer>
     );
 };
 
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,17 +6,32 @@ import {
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
     signOut,
-    onAuthStateChanged
+    onAuthStateChanged,
 } from "firebase/auth";
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc,
+    collection,
+    writeBatch,
+    query,
+    getDocs,
+} from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBBKAcgA85mRQUICPSd0APbGo9Hc-1sWGk",
-    authDomain: "crwn-clothing-db-8144b.firebaseapp.com",
-    projectId: "crwn-clothing-db-8144b",
-    storageBucket: "crwn-clothing-db-8144b.appspot.com",
-    messagingSenderId: "887323547295",
-    appId: "1:887323547295:web:fa10fc3948d6488f9db4aa",
+    // apiKey: "AIzaSyBBKAcgA85mRQUICPSd0APbGo9Hc-1sWGk",
+    // authDomain: "crwn-clothing-db-8144b.firebaseapp.com",
+    // projectId: "crwn-clothing-db-8144b",
+    // storageBucket: "crwn-clothing-db-8144b.appspot.com",
+    // messagingSenderId: "887323547295",
+    // appId: "1:887323547295:web:fa10fc3948d6488f9db4aa",
+    apiKey: "AIzaSyCJzWZ-30Ijlhd3pD7H-kn2zSFHYwop51g",
+    authDomain: "crwn-clothing-db-1-a9492.firebaseapp.com",
+    projectId: "crwn-clothing-db-1-a9492",
+    storageBucket: "crwn-clothing-db-1-a9492.appspot.com",
+    messagingSenderId: "4673735909",
+    appId: "1:4673735909:web:6a62e68b55bb66c5e8980c",
 };
 
 // Initialize Firebase
@@ -28,12 +43,48 @@ googleProvider.setCustomParameters({
 });
 
 export const auth = getAuth(firebaseApp);
+
 export const signInWithGooglePopup = () =>
     signInWithPopup(auth, googleProvider);
+
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth, additionalInfo = {}) => {
-    if(!userAuth) return;
+export const addCollectionAndDocuments = async (
+    collectionKey,
+    objectsToAdd
+) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach((object) => {
+        const docRef = doc(collectionRef, object.title.toLowerCase());
+        batch.set(docRef, object);
+    });
+
+    await batch.commit();
+    console.log("Done");
+};
+
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, "categories");
+
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+        const { title, items } = docSnapshot.data();
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {});
+
+    return categoryMap;
+};
+
+export const createUserDocumentFromAuth = async (
+    userAuth,
+    additionalInfo = {}
+) => {
+    if (!userAuth) return;
     const userDocRef = doc(db, "users", userAuth.uid);
     const userSnapshot = await getDoc(userDocRef);
     if (!userSnapshot.exists()) {
@@ -45,7 +96,7 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInfo = {}) 
                 displayName,
                 email,
                 createdAt,
-                ...additionalInfo
+                ...additionalInfo,
             });
         } catch (error) {
             console.log("error creating a user ", error.message);
@@ -66,4 +117,5 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
 export const signOutUser = async () => await signOut(auth);
 
-export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
+export const onAuthStateChangedListener = (callback) =>
+    onAuthStateChanged(auth, callback);
